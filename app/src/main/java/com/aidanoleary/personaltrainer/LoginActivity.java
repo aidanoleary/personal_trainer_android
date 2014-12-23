979b8162f520177144887d6f86c4a7aac5d9cf88
aidanoleary <aidanoleary92@gmail.com>
2014-12-23 18:34:16 +0000

    Added Login and signup activities

--- app/src/main/java/com/aidanoleary/personaltrainer/LoginActivity.java
+++ app/src/main/java/com/aidanoleary/personaltrainer/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.aidanoleary.personaltrainer;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,30 +15,109 @@ import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected Button mLoginButton;
+    protected TextView mSignUpText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Enable progress indicator
+        //RequestWindowFeature needs to be called before setContentView()
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
         setContentView(R.layout.activity_login);
 
-        //Get layout items
-        EditText usernameInput = (EditText) findViewById(R.id.loginUsernameText);
-        EditText passwordInput = (EditText) findViewById(R.id.loginPasswordText);
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-        TextView signUpText = (TextView) findViewById(R.id.loginSignupTextView);
+        //Get user interface objects
+        mUsername = (EditText) findViewById(R.id.loginUsernameField);
+        mPassword = (EditText) findViewById(R.id.loginPasswordField);
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+        mSignUpText = (TextView) findViewById(R.id.loginSignupText);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        //Set onclick listener for the login button
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Log.v("login", "login button clicked");
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+
+                username = username.trim();
+                password = password.trim();
+
+                //Check if Username and Password is empty
+                if(username.isEmpty() || password.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setMessage(R.string.login_error_message);
+                    builder.setTitle(R.string.login_error_title);
+                    builder.setPositiveButton(android.R.string.ok, null);
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else {
+                    // Login
+                    //Make progress indicator visible
+                    setProgressBarIndeterminateVisibility(true);
+
+
+
+
+
+
+
+                    //Don't forget to set the progress bar to false
+                    //setProgressBarIndeterminateVisibility(false);
+
+
+
+
+
+
+                    /*
+                    ParseUser.logInInBackground(username, password, new LogInCallback() {
+                        @Override
+                        public void done(ParseUser parseUser, ParseException e) {
+                            //Hide progress indicator
+                            setProgressBarIndeterminateVisibility(false);
+
+                            if (e == null) {
+                                //Login was successful
+                                // The new user was successfully created
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage(e.getMessage());
+                                builder.setTitle(R.string.login_error_title);
+                                builder.setPositiveButton(android.R.string.ok, null);
+
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+                    */
+                }
             }
         });
 
-        signUpText.setOnClickListener(new View.OnClickListener() {
+
+        //Set the onclick listener for signup text
+        mSignUpText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v("sign up", "sign up text clicked");
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
             }
         });
+
+
+
+
     }
 
 
--- app/src/main/java/com/aidanoleary/personaltrainer/SignupActivity.java
+++ app/src/main/java/com/aidanoleary/personaltrainer/SignupActivity.java
@@ -1,23 +1,110 @@
 package com.aidanoleary.personaltrainer;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class SignupActivity extends Activity {
 
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected EditText mEmail;
+    protected Button mSignupButton;
+    protected TextView mLoginText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_signup);
 
-        //Get signup fields
-        EditText usernameField = (EditText) findViewById(R.id.signupUsernameText);
-        EditText passwordField = (EditText) findViewById(R.id.signupPasswordText);
 
+        //Get User interface objects
+        mUsername = (EditText) findViewById(R.id.signupUsernameField);
+        mPassword = (EditText) findViewById(R.id.signupPasswordField);
+        mEmail = (EditText) findViewById(R.id.signupEmailField);
+        mSignupButton = (Button) findViewById(R.id.signupButton);
+        mLoginText = (TextView) findViewById(R.id.signupLoginText);
+
+        //Add an onclick listener for the signup button
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+                String email = mEmail.getText().toString();
+
+                username = username.trim();
+                password = password.trim();
+                email = email.trim();
+
+                //Check if any of the fields are empty
+                if(username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
+                    builder.setMessage(R.string.signup_error_message);
+                    builder.setTitle(R.string.signup_error_title);
+                    builder.setPositiveButton(android.R.string.ok, null);
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else {
+                    // Create the new user using Async task
+                    Log.v("Hello", "signup success");
+
+                    /*
+                    setProgressBarIndeterminateVisibility(true);
+
+                    ParseUser newUser = new ParseUser();
+                    newUser.setUsername(username);
+                    newUser.setPassword(password);
+                    newUser.setEmail(email);
+                    newUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            setProgressBarIndeterminateVisibility(false);
+
+                            if(e == null) {
+                                // The new user was successfully created
+                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                                builder.setMessage(e.getMessage());
+                                builder.setTitle(R.string.signup_error_title);
+                                builder.setPositiveButton(android.R.string.ok, null);
+
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+                    */
+                }
+            }
+        });
+
+        mLoginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
