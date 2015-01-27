10693aa4c422caaa4803f0adde2d03550edd78c5
aidanoleary <aidanoleary92@gmail.com>
2015-01-27 18:10:29 +0000

    Worked on the GenerateRoutineActivity.

--- app/src/main/java/com/aidanoleary/personaltrainer/GenerateRoutineActivity.java
+++ app/src/main/java/com/aidanoleary/personaltrainer/GenerateRoutineActivity.java
@@ -1,17 +1,97 @@
 package com.aidanoleary.personaltrainer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class GenerateRoutineActivity extends Activity {
+
+    private int currentQuestionNumber;
+    private String[] questionList;
+    private TextView questionNumberText;
+    private TextView questionText;
+    private RadioGroup answersGroup;
+    private Button confirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_routine);
+
+        // Initialise the list of questions
+        questionList = new String[]{
+                "How fit do you consider yourself?",
+                "Are you male or female?",
+                "How many times a week do you exercise?",
+                "What is your main goal?",
+        };
+
+        // Set the question number to 1.
+        currentQuestionNumber = 1;
+
+        // Get Interface Items
+        questionNumberText = (TextView) findViewById(R.id.generateQuestionNumberText);
+        questionText = (TextView) findViewById(R.id.generateQuestionText);
+        answersGroup = (RadioGroup) findViewById(R.id.generateAnswersRadioGroup);
+        confirmButton = (Button) findViewById(R.id.generateConfirmButton);
+
+        // Initialise first values of on screen objects
+        // ===========================================
+        // Set the question number text with the current question
+        questionNumberText.setText(questionNumberText.getText().toString() + currentQuestionNumber);
+        // Set the questionText to the first question in the question list
+        questionText.setText(questionList[0]);
+        // Add the answer radio buttons
+
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Use a switch statement to determine what action should be performed.
+                switch (currentQuestionNumber) {
+                    // Question 1 has been submitted
+                    case 1:
+                        // Get users selections and perform correct actions.
+
+                        // Increase current question number
+                        currentQuestionNumber++;
+
+                        // Update Display contents
+                        // Change the question number and question text
+                        questionNumberText.setText("Question: " + currentQuestionNumber);
+                        questionText.setText(questionList[currentQuestionNumber]);
+                        break;
+
+                    default:
+                        // Update the Database
+
+                        // Load the Main Activity
+                        // Set flags so user can't retreat
+                        Intent intent = new Intent(GenerateRoutineActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        break;
+
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 
 
--- app/src/main/java/com/aidanoleary/personaltrainer/SettingsFragment.java
+++ app/src/main/java/com/aidanoleary/personaltrainer/SettingsFragment.java
@@ -1,12 +1,14 @@
 package com.aidanoleary.personaltrainer;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 
 
 /**
@@ -18,6 +20,34 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        //Add a onclick listener to the generate workout button
+        rootView.findViewById(R.id.settingsGenerateButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create a dialog box that ask the user to confirm the generation of a new workout.
+                // Use the Builder class for convenient dialog construction
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage("Are you sure you wish to generate a new workout?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User has said they want to generate a new workout
+                                Intent intent = new Intent(getActivity(), GenerateRoutineActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        });
+                // Create the AlertDialog object and return it
+                builder.create();
+                builder.show();
+            }
+        });
+
         return rootView;
+
     }
 }
