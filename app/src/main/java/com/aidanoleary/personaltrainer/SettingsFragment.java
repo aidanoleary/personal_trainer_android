17296fc1a0a6865c80adffd8992c30e4cff28375
aidanoleary <aidanoleary92@gmail.com>
2015-04-01 23:24:02 +0100

    Made generate workout button in setting invisible till the feature to generate a new workout is implemented.

--- app/src/main/java/com/aidanoleary/personaltrainer/SettingsFragment.java
+++ app/src/main/java/com/aidanoleary/personaltrainer/SettingsFragment.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -21,8 +22,10 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_settings, container, false);
 
+        Button generateButton = (Button) rootView.findViewById(R.id.settingsGenerateButton);
+
         //Add a onclick listener to the generate workout button
-        rootView.findViewById(R.id.settingsGenerateButton).setOnClickListener(new View.OnClickListener() {
+        generateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Create a dialog box that ask the user to confirm the generation of a new workout.
@@ -48,6 +51,12 @@ public class SettingsFragment extends Fragment {
                 builder.show();
             }
         });
+
+        // Hide the button and don't make it clickable because this functionality hasn't been implemented
+        // yet and I don't know what will happen if we try to generate a workout for a user who already has one.
+        generateButton.setVisibility(View.INVISIBLE);
+        generateButton.setClickable(false);
+
 
         return rootView;
 
